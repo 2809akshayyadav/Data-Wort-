222e5866a00d43dcdc3c86100155c28b7a6d3a41
unknown <2809akshayyadav@gmail.com>
2024-09-08 17:18:23 +0530

    Data Worth

--- team.php
+++ team.php
@@ -53,17 +53,17 @@ include '.\include\header.html';
               </div>
               <div class="row">
                 <div class="col-md-6 col-sm-12 fx animated fadeInUp management" data-animate="fadeInUp">
-                  <div class="member-pp"><img src="images/ashok-anibha.jpg" alt=""></div>
-                  <h1 class="text-center">Ashok Anibha</h1>
+                  <div class="member-pp"><img src="images\IMG-20240103-WA0005.jpg" alt=""></div>
+                  <h1 class="text-center">Akshay Yadav</h1>
                   <div class="text-center"><strong>General Manager,</strong></div>
                   <div class="text-center"><strong>Technology and Operations, Director (Based in Noida, India)</strong>
                   </div>
-                  <p class="text-left">Founder, Shareholder and Director Ashok is one of four Founders of Girikon. Ashok
-                    has over 20 years experience in design, development &amp; deployment of complex, large scale
+                  <p class="text-left">Founder, Shareholder and Director Akshay is one of four Founders of Data-Worth. Akshay
+                    has over 4 years experience in design, development &amp; deployment of complex, large scale
                     information technology solutions for the Manufacturing, Financial, FMCG industries. He has
                     previously served as Vice President, Engineering at Agnicient Technologies, and Product architect at
                     Ideactive Consulting.</p>
-                  <p class="text-left">Ashok is an enthusiastic supporter of startups and is an angel investor in
+                  <p class="text-left">Akshay is an enthusiastic supporter of startups and is an angel investor in
                     TeamIndus, JikkoLabs amongst other path breaking startups from India.</p> <span><strong>What they
                       say:</strong> His team calls him a Problem solver, Communicator, team player and is always ready
                     to mentor and groom talent. All his customers count on him to find the right solution, deliver on
@@ -83,65 +83,6 @@ include '.\include\header.html';
                     leadership, vast knowledge and extensive experience across various industries and technologies.</p>
                   <span><strong>Great joke Sachin!</strong> Sachin’s sense of humour and energetic persona never fail to
                     make the working environment an enjoyable experience. What a great place to work!</span>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-md-6 col-sm-12 fx animated fadeInUp management" data-animate="fadeInUp">
-                  <div class="member-pp"><img src="images/alankar-anibha.jpg" alt=""></div>
-                  <h1 class="text-center">Alankar Anibha </h1>
-                  <div class="text-center"><strong>Delivery and Service,</strong></div>
-                  <div class="text-center"><strong>Director (Based in Phoenix, USA) </strong></div>
-                  <p class="text-left">Founder, Shareholder and Director, is one of four team members who founded
-                    Girikon. Alankar has 20+ experience in Information Technology. Starting as an avid Java Programmer,
-                    Alankar has played multiple roles in the IT Services industry in different Industry domains and
-                    Technology Platforms. Prior to Girikon, Alankar managed many Enterprise level software
-                    implementations focussing on the Digital and Information Security space.</p>
-                  <p class="text-left">Alankar’s patient approach to complex problem, attention to detail, focused
-                    program execution and desire to providing Girikon’s customers with a quality outcome are all of
-                    which makes Girikon an ideal partner and the customers keep on coming back.</p> <span><strong>Desire
-                      for a quality outcome:</strong> Alankar’s deep understanding of different Technology Platforms and
-                    Program Delivery in general has established the foundation needed to deliver on a set of
-                    deliverables and achieve the best possible outcome for the customer.</span>
-                </div>
-                <div class="col-md-6 col-sm-12 fx animated fadeInUp management" data-animate="fadeInUp">
-                  <div class="member-pp"><img src="images/alok-anibha.jpg" alt=""></div>
-                  <h1 class="text-center">Alok Anibha</h1>
-                  <div class="text-center"><strong>Head, Salesforce Practice,</strong></div>
-                  <div class="text-center"><strong>Director (Based in Noida, India) </strong></div>
-                  <p class="text-left">Founder, Shareholder and Director, is one of four team members who founded
-                    Girikon. Alok has many years’ experience in the software industry. Previously he was Quality and
-                    Support Manager at Tribal Group, Project Delivery Manager at Agnicient Technologies and Ideactive.
-                    Alok spent 15+ years building his reputation as a true software professional with a constant eye on
-                    quality delivery and working to delight an impressive list customers including Ring Central,
-                    Omnicom, HP, ABN Amro Bank, L’Oreal, Bennett Coleman &amp; Co, British Council to name a few.</p>
-                  <span><strong>Technical Prowess:</strong> Alok’s technical chops are exceptional and he has a deep
-                    expertise in translating high level requirements and boiling this down into actionable development
-                    results.</span>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-md-6 col-sm-12 col-sm-offset-3 fx animated fadeInUp management" data-animate="fadeInUp">
-                  <div class="member-pp"><img src="images/yiannis-loupis.jpg" alt=""></div>
-                  <h1 class="text-center">Yiannis Loupis</h1>
-                  <div class="text-center"><strong>Managing Director,</strong></div>
-                  <div class="text-center"><strong>Australia and New Zealand (Based in Melbourne, Australia)</strong>
-                  </div>
-                  <p class="text-left">Shareholder and Director, Joined Girikon in late 2018. Yiannis has over 20 years
-                    of experience in Business Management/Ownership and delivering complex Programs in multiple
-                    industries including Information Technology and Education. Previously as Portfolio Director, Yiannis
-                    established the Portfolio Management Office in the ANZ region after a series of acquisitions by
-                    Tribal Group, a global leader in the software and services. His exceptional skills and experience in
-                    personnel management, operations management, program management, financial budgeting, strategic
-                    planning, business modelling, stakeholder and vendor management is only a handful of skills in his
-                    kitbag. Girikon has given him the opportunity to expand its operations and provide an above standard
-                    profession services in the Aust/NZ region.</p> <span><strong>Incredible Girikon!!</strong> Girikon
-                    has a team of professional, dedicated and focused individuals. Results and outcomes are what count
-                    and Girikon has proven themselves in a relatively short time to consistently deliver quality results
-                    for their customers. Thanks to management and the whole team for the warm welcome into the Girikon
-                    team. There is a real sense of pride and ownership in the work, projects and growth achieved to date
-                    and a firm excitement about the future of Girikon.</span>
-                  <p class="text-left"><strong>I can’t wait for our prospective clients in Australia and New Zealand to
-                      experience this!</strong></p>
                 </div>
               </div>
             </div>
